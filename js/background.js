4a6bb9a952ccd4589f6f4fcb24e0aca44d547464
Ruyago <ruyago@gmail.com>
2022-10-31 10:43:20 +0100

    test

--- js/background.js
+++ js/background.js
@@ -1,22 +1,20 @@
 class Background {
     draw() {
-        game.backgroundImages.forEach(function(img) {
+        /*game.backgroundImages.forEach(function(img) {
             img.x -= img.speed
             image(img.src, img.x, 0, width, height)
             image(img.src, img.x + width, 0, width, height)
             if (img.x <= -width) img.x = 0
-        })
+        })*/
     }
 }
-  this.background = new Background()
-        this.player = new Player()
-        this.obstacles = [];
+    this.background = new Background()
+        
     
     preload() {
         this.backgroundImages = [
-            { src: loadImage("../assets/background/road.jpg"), x: 0, speed: 0 },
+            { src: loadImage("../assets/background/road.png"), x: 0, speed: 0 },
            
         ]
-        this.playerImage = loadImage("../assets/player/bb8.gif")
-        this.coinImage = loadImage("../assets/coins/tile000.png")
+
     }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function setup() {
-    createCanvas(710, 400);
+    createCanvas(800, 400);
  }
 
 
