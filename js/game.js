9c8e2519d048c259d6f8ef4c92088dc63edb520a
Jbilenrijeka <115556679+Jbilenrijeka@users.noreply.github.com>
2022-10-31 10:50:47 +0100

    class Game add

---
+++ js/game.js
@@ -0,0 +1,9 @@
+class Game {
+	constructor() 
+     this.car = new Car() 
+      this.background = new.background()
+       this.backgroundImages
+       this.createImage
+       this.obstacles 
+}
+
